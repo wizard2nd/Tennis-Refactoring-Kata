6e6b6ba1ed9640c283c300df12520adb2306361e
Jakub Adler <adler.jakub@gmail.com>
2018-07-04 18:50:15 +0100

    improve readability by renaming variables which keeps number of points

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -127,29 +127,29 @@ class TennisGame3
   def initialize(player1Name, player2Name)
     @player1_name = player1Name
     @player2_name = player2Name
-    @p1 = 0
-    @p2 = 0
+    @player1_points = 0
+    @player2_points = 0
   end
 
   def won_point(n)
     if n == @player1_name
-        @p1 += 1
+        @player1_points += 1
     else
-        @p2 += 1
+        @player2_points += 1
     end
   end
 
   def score
-    if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6) # one or other player leads
+    if (@player1_points < 4 and @player2_points < 4) and (@player1_points + @player2_points < 6) # one or other player leads
       p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@p1]
-      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2] # draw or leading
+      s = p[@player1_points]
+      @player1_points == @player2_points ? s + "-All" : s + "-" + p[@player2_points] # draw or leading
     else
-      if (@p1 == @p2)
+      if (@player1_points == @player2_points)
         "Deuce"
       else
-        s = @p1 > @p2 ? @player1_name : @player2_name
-        (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+        s = @player1_points > @player2_points ? @player1_name : @player2_name
+        (@player1_points-@player2_points)*(@player1_points-@player2_points) == 1 ? "Advantage " + s : "Win for " + s
       end
     end
   end
